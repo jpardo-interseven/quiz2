b840c071d3bf0b9b94ef1a98524841b23589e890
Jose Pardo <jpardo-interseven@users.noreply.github.com>
2015-06-24 21:51:59 +0200

    Cambio de success por then

--- models/models.js
+++ models/models.js
@@ -32,14 +32,14 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exporta la definición de la tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en la DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count) {
+  Quiz.count().then(function(count) {
     if (count === 0) {  // la tabla se inicializa sólo si está vacía.
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
                  })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
